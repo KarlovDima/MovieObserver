e24abdf064119d74ddf0c575d084250663d966c3
Dima Karlov <karlova1@gmail.com>
2018-07-11 15:00:04 +0300

    Add Lombok to models

--- manager/src/main/java/com/dima/models/Film.java
+++ manager/src/main/java/com/dima/models/Film.java
@@ -1,50 +1,16 @@
 package com.dima.models;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class Film {
     private int id;
     private String name;
     private int duration;
     private boolean isActive;
-
-    public Film() {
-    }
-
-    public Film(int id, String name, int duration, boolean isActive) {
-        this.id = id;
-        this.name = name;
-        this.duration = duration;
-        this.isActive = isActive;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(boolean active) {
-        isActive = active;
-    }
 }
--- manager/src/main/java/com/dima/models/FilmScreening.java
+++ manager/src/main/java/com/dima/models/FilmScreening.java
@@ -1,40 +1,15 @@
 package com.dima.models;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class FilmScreening {
     private int id;
     private int filmId;
     private String time;
-
-    public FilmScreening() {
-    }
-
-    public FilmScreening(int id, int filmId, String time) {
-        this.id = id;
-        this.filmId = filmId;
-        this.time = time;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getFilmId() {
-        return filmId;
-    }
-
-    public void setFilmId(int filmId) {
-        this.filmId = filmId;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public void setTime(String time) {
-        this.time = time;
-    }
 }
